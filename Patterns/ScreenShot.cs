9802c19f4cd90d4208f8d9c964aa827658b66a7d
jany4 <ivanvorobyov@coherentsolutions.com>
2020-06-04 18:23:42 +0300

    Allure reporting system added

---
+++ Patterns/ScreenShot.cs
@@ -0,0 +1,17 @@
+﻿
+using OpenQA.Selenium;
+
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+
+namespace Patterns
+{
+    public class ScreenShot
+    {
+        public static byte[] Take(IWebDriver driver)
+        {
+            return ((ITakesScreenshot)driver).GetScreenshot().AsByteArray;
+        }
+    }
+}
--- Patterns/UnitTest1.cs
+++ Patterns/UnitTest1.cs
@@ -1,13 +1,18 @@
 using NUnit.Framework;
 using System;
+using System.IO;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support;
+using OpenQA.Selenium.Support.Extensions;
 using SeleniumExtras.PageObjects;
+using Allure.NUnit.Attributes;
+using Allure.Commons;
 
 namespace Patterns
 {
-    public class Tests
+    
+
+    public class Tests : AllureReport
     {
         private static IWebDriver _driver;
 
@@ -17,13 +22,27 @@ namespace Patterns
             _driver = new ChromeDriver();
         }
 
+        
         [TearDown]
         public void CleanUp()
         {
+            string result = TestContext.CurrentContext.Result.Outcome.Status.ToString();
+            
+            if (TestContext.CurrentContext.Result.Outcome.Status.ToString() == "Failed")
+            {
+                AllureLifecycle.Instance.AddAttachment("Fail screenshot", AllureLifecycle.AttachFormat.ImagePng, ScreenShot.Take(_driver));
+                //AllureLifecycle.Instance.AddAttachment("Fail Screenshot", AllureLifecycle.AttachFormat.ImagePng, ((ITakesScreenshot)_driver).GetScreenshot().AsByteArray);
+            }
+                
             _driver.Close();
         }
 
-        [TestCase("seleniumtests2", "123456789zxcvbn")]
+        [AllureTest("Testing Login functionality")]
+        [AllureLink("https//:test.com/issue1", false)]
+        [AllureSeverity(Allure.Commons.Model.SeverityLevel.Critical)]
+        [AllureSubSuite("Login")]
+        [AllureOwner("Vorobyov")]
+        [TestCase("seleniumtests2", "123456789zxcvbn1")]//wrong test data to get error in the report
         [Test]
         public void LogInTest(string logIn, string password)
         {
@@ -33,6 +52,11 @@ namespace Patterns
             Assert.IsTrue(tutByLoginPage.LogIn(logIn, password).IsLoaded);
         }
 
+        [AllureTest("Testing Logout functionality")]
+        [AllureLink("issue2")]
+        [AllureSeverity(Allure.Commons.Model.SeverityLevel.Normal)]
+        [AllureSubSuite("Logout")]
+        [AllureOwner("Vorobyov")]
         [TestCase("seleniumtests2", "123456789zxcvbn")]
         [Test]
         public void LogOutTest(string logIn, string password)
